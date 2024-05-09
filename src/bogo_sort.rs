54c30b00ca371923095c064961f5bd333710a5ef
Hugo Brucker <hugo.brucker@gmail.com>
2024-05-09 10:06:15 +0200

    Factorisation of code in lib.rs

--- src/bogo_sort.rs
+++ src/bogo_sort.rs
@@ -1,4 +1,5 @@
 use rand::{Rng, thread_rng};
+use crate::is_sorted;
 
 fn random_shake<T: PartialOrd>(tab: &mut [T], len: usize) {
     let mut rng = thread_rng();
@@ -7,15 +8,6 @@ fn random_shake<T: PartialOrd>(tab: &mut [T], len: usize) {
         let rdm_pos  = rng.gen_range(0..len);
         tab.swap(pos, rdm_pos);
     }
-}
-
-fn is_sorted<T: PartialOrd>(tab: &[T], len: usize) -> bool {
-    for pos in 0..(len - 1) {
-        if tab[pos] > tab[pos + 1] {
-            return false;
-        };
-    };
-    true
 }
 
 fn bogo_sort<T: PartialOrd>(tab: &mut [T], len: usize) {
@@ -27,18 +19,6 @@ fn bogo_sort<T: PartialOrd>(tab: &mut [T], len: usize) {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn sorted_array() {
-        let array = [1, 2, 3, 4];
-        assert!(is_sorted(&array, 4));
-    }
-    
-    #[test]
-    fn unsorted_array() {
-        let array = [4, 8, 6, 3];
-        assert!(!is_sorted(&array, 4));
-    }
 
     #[test]
     fn bogo_test() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 pub mod bogo_sort;
+pub mod bubble_sort;
+
+fn is_sorted<T: PartialOrd>(tab: &[T], len: usize) -> bool {
+    for pos in 0..(len - 1) {
+        if tab[pos] > tab[pos + 1] {
+            return false;
+        };
+    };
+    true
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn sorted_array() {
+        let array = [1, 2, 3, 4];
+        assert!(is_sorted(&array, 4));
+    }
+    
+    #[test]
+    fn unsorted_array() {
+        let array = [4, 8, 6, 3];
+        assert!(!is_sorted(&array, 4));
+    }
 }
